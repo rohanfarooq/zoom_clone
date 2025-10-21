17f0ec20487ddafca96393a0477b9860744ce1aa
Rohan Farooq <rohanfarooq473@gmail.com>
2025-10-21 19:10:57 +0500

    .

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -19,7 +19,7 @@ const Table = ({
       <h1 className="text-base font-medium text-sky-1 lg:text-xl xl:min-w-32">
         {title}:
       </h1>
-      <h1 className="truncate text-sm font-bold max-sm:max-w-[320px] lg:text-xl">
+      <h1 className="truncate text-sm font-bold max-w-[calc(100%-8rem)] max-md:max-w-[320px] lg:text-xl">
         {description}
       </h1>
     </div>
